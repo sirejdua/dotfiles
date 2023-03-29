3c738c57f9870247446173d7b6500145de078cff
Sirej Dua <sirejdua@gmail.com>
2023-03-29 13:47:28 -0700

    dotfiles on manjaro

--- lspconfig.lua
+++ lspconfig.lua
@@ -1,75 +1,3 @@
-vim.opt.termguicolors = true
-vim.opt.syntax = 'on'
-vim.opt.errorbells = false
-vim.opt.smartcase = true
-vim.opt.showmode = false
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = vim.fn.stdpath('config') .. '/undodir'
-vim.opt.undofile = true
-vim.opt.incsearch = true
-vim.opt.hidden = true
-vim.opt.completeopt='menuone,noinsert,noselect'
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.signcolumn = 'yes'
-vim.opt.wrap = false
-vim.api.nvim_set_keymap('n', 'Y', 'Y', {noremap = true})
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-local vim = vim
-local execute = vim.api.nvim_command
-local fn = vim.fn
--- ensure that packer is installed
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-    execute 'packadd packer.nvim'
-end
-vim.cmd('packadd packer.nvim')
-local packer = require'packer'
-local util = require'packer.util'
-packer.init({
-  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
-})
-
-
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'sheerun/vim-polyglot'
-  -- these are optional themes but I hear good things about gloombuddy ;)
-  -- colorbuddy allows us to run the gloombuddy theme
-  use 'tjdevries/colorbuddy.nvim'
-  use 'bkegley/gloombuddy'
-  -- sneaking some formatting in here too
-  use {'prettier/vim-prettier', run = 'yarn install' }
-  use 'simrat39/rust-tools.nvim'
-  end
-)
-
-vim.g.colors_name = 'gloombuddy'
-
-local configs = require'nvim-treesitter.configs'
-configs.setup {
-  auto_install = true,
-  highlight = {
-    enable = true,
-  }
-}
-
-
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -112,7 +40,7 @@ require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
-require('lspconfig')['lua_ls'].setup{
+require('lspconfig')['lua-language-server'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
     settings = {
@@ -135,17 +63,3 @@ require('lspconfig')['rust_analyzer'].setup{
       ["rust-analyzer"] = {}
     }
 }
-
-local rt = require("rust-tools")
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
-
